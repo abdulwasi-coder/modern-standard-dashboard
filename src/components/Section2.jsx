fa33b230b731eecf3694e7ee45acbcbbf2b7a6cf
Abdul Wasi <abdulwasishirzai567@gmail.com>
2026-03-28 08:40:23 +0430

    changing text and some style

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -4,7 +4,7 @@ const Section2 = () => {
             <h1 id="header">About Me</h1>
             <p>I am a creative modern and passionate UI/UX developer and have a knack for creating modern and user-friendly
                 websites. I love turning ideas into reality through code and design. I am constantly adding new technologies
-                to improve the React web. When I am not coding, you can put a message and then I will respond in less then 30 minutes.</p>
+                to improve the React web. When I am busy, you can put a message and then I will respond in less then 30 minutes.</p>
         </section>
     )
 }
